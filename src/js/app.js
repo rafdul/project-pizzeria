984d3214ac5a23f27141a5679d9fa23650f05a71
Raf Dul <rafalduluk@gmail.com>
2020-11-05 12:42:09 +0100

    Add subbpage 'Booking'

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,70 @@ import Cart from './components/Cart.js';
 // deklaracja wykorzystywanych metod dla zmiennej app czyli obiektu,
 // który pomoże w organizacji kodu aplikacji,
 const app = {
+  // metoda przełączania podstron (bez przeładowywania strony)
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log('idFromHash', idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    console.log('pageMatchingHash', pageMatchingHash);
+
+    // uruchomienie metody aktywacji podstrony pierwszej (czyli o indeksie 0 w tablicy)
+    // znalezionej w selektorze thisApp.pages; konieczne wydobycie id, bo w html tylko tym atrybutem są one różnicowane
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        // run thisApp.activatePage with that id
+        thisApp.activatePage(id);
+
+        // change URL hash (czyli końcówka URL po #)
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    // add class active to matching page, remove from non-matching
+    for(let page of thisApp.pages){
+      // if(page.id == pageId){
+      //   page.classList.add(classNames.pages.activate);
+      // } else {
+      //   page.classList.remove(classNames.pages.activate);
+      // }
+
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    // add class active to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     // console.log('thisApp.data:', thisApp.data); // .data to odesłanie do pliku data.js
@@ -64,6 +128,7 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+    thisApp.initPages();
     thisApp.initData();
     // thisApp.initMenu();
     thisApp.initCart();
