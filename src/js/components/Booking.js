ef7be69f76fb154de868811a82d4301d443291b7
Raf Dul <rafalduluk@gmail.com>
2020-11-11 12:41:43 +0100

    Get data about bookings and events from API and make urls

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {select, templates} from '../settings.js';
+import {select, settings, templates} from '../settings.js';
+import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -10,6 +11,74 @@ class Booking{
     // const bookingElement = thisBooking.querySelector(select.containerOf.booking);
     thisBooking.render(containerOfBooking);
     thisBooking.initWidget();
+    thisBooking.getData();
+  }
+
+  getData(){
+    const thisBooking = this;
+
+    const stardDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        stardDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        stardDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    // console.log('getData params', params);
+
+    const urls = {
+      booking:       settings.db.url + '/' + settings.db.booking
+                                     + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsRepeat.join('&'),
+    };
+    // console.log('get Data urls', urls);
+
+    // fetch(urls.booking)
+    //   .then(function(bookingsResponse){
+    //     return bookingsResponse.json();
+    //   })
+    //   .then(function(bookings){
+    //     console.log(bookings);
+    //   });
+
+    // Metoda Promise.all zbiera funkcje fetch dla wielu argumentów i wykonuje je po kolei
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const bookingseventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          bookingseventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
+
+
   }
 
   render(containerOfBooking){
